275b1d76b553db8e8adfa8ae53adf3c0c0fa96d9
VeghFruzsi <vefruzso88@gmail.com>
2018-10-27 19:54:24 +0200

    commit commit

--- week-1/day02/12_cuboid/main.cpp
+++ week-1/day02/12_cuboid/main.cpp
@@ -1,12 +1,33 @@
+
 #include <iostream>
 
-int main(int argc, char* args[]) {
 
     // Write a program that stores 3 sides of a cuboid as variables (doubles)
     // The program should write the surface area and volume of the cuboid like:
-    // 
+    //
     // Surface Area: 600
     // Volume: 1000
 
-    return 0;
+int main(int argc, char* args[]) {
+
+    double l, w, h, area, volume;
+
+    std::cout << "Please, enter the lenght:"  << std::endl;
+    std::cin >> l ;
+
+    std::cout << "Please, enter the width:"  << std::endl;
+    std::cin >> w ;
+
+    std::cout << "Please, enter the height:"  << std::endl;
+    std::cin >> h ;
+
+    volume = l * w * h ;
+
+    area = 2 * (l * w + w * h + l * h);
+
+    std::cout << "Cuboid's volume is: " << volume << std::endl;
+    std::cout << "Cuboid's area is: " << area << std::endl;
+
+
+return 0;
 }
