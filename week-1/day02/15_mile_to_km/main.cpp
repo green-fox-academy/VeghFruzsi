d7c269b1ad4bc8386c8153930bb02d8065e33c25
VeghFruzsi <vefruzso88@gmail.com>
2018-10-27 20:38:00 +0200

    km miles calc

---
+++ week-1/day02/15_mile_to_km/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    // Write a program that asks for an integer that is a distance in kilometers,
+    // then it converts that value to miles and prints it
+
+    int lengthInKm,lengthInMiles;
+
+    std::cout << "Please, add a length of your trip in kilometers: " << std::endl;
+    std::cin >> lengthInKm ;
+
+    lengthInMiles = lengthInKm * 62 / 100;
+
+    std::cout << "Your trip is: " << lengthInMiles << " in miles." << std::endl;
+
+    return 0;
+}
+
