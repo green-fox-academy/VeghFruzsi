e3ce0023bd64fbfd1c8a7d9b917e43feb2a98221
VeghFruzsi <vefruzso88@gmail.com>
2018-10-27 20:25:00 +0200

    howdoyoudo

--- week-1/day02/14_hello_user/main.cpp
+++ week-1/day02/14_hello_user/main.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char* args[]) {
 
-    // Modify this program to greet user instead of the World!
-    // The program should ask for the name of the user
-    std::cout << "Hello world" << std::endl;
-
-    int userName;
-    std::cout << "Please enter your name: ";
+    std::string userName;
+    std::cout << "Please enter your name: " << std::endl;
     std::cin >> userName;
 
-    std::string colorOfCurtain;
-    std::cout << "Please enter the color of the curtain (string): ";
-    std::cin >> colorOfCurtain;
-
-    std::cout << "You can see " << numberOfWindows << " windows from your place with beautiful " << colorOfCu
+    std::cout << " Hello, " << userName << "! How do you do?" << std::endl;
 
     return 0;
 }
