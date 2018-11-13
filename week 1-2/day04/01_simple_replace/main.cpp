2ca5ac8f0a11399a37bbbb4781541c4667815860
VeghFruzsi <vefruzso88@gmail.com>
2018-11-13 22:29:11 +0100

    update

--- week 1-2/day04/01_simple_replace/main.cpp
+++ week 1-2/day04/01_simple_replace/main.cpp
@@ -8,12 +8,29 @@ int main(int argc, char* args[])
     // Please fix it for me!
     // Expected output: In a galaxy far far away
 
+
+    //first variation
     std::string example = "In a dishwasher far far away";
     std::string wrongWord = "dishwasher";
     std::string replace = "galaxy";
 
     example.replace(example.find(wrongWord),wrongWord.size(),replace);
     std::cout << example << std::endl;
+
+
+    //second variation
+    std::string sentence = "In a dishwasher far far away";
+    std::string from = "dishwasher";
+
+    int start_pos = sentence.find(from);
+
+    if (start_pos == std::string::npos) {
+        std::cout << "Cannot replace" << std::endl;
+    }else{
+        sentence.replace(start_pos, from.length(), "galaxy");
+    }
+
+    std::cout << sentence << std::endl;
 
     return 0;
 }
