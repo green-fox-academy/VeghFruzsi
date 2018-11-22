29d071f7a1db16b3a9877a2caace970e432fe20d
VeghFruzsi <vefruzso88@gmail.com>
2018-11-22 22:49:21 +0100

    update

--- week 3/day01/02_print_value/main.cpp
+++ week 3/day01/02_print_value/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
-int main ()
-{
+int main() {
     // Print out the value of the variable called 'age' using a pointer
     // (you have to create the pointer first)
 
@@ -12,11 +11,12 @@ int main ()
     std::cout << "The memory address of age is " << &age << std::endl;
     std::cout << "The value of agePtr is " << agePtr << std::endl;
 
-
     agePtr = &age;
 
+    std::cout << "The value of age is " << age << std::endl;
+    std::cout << "The memory address of age is " << &age << std::endl;
     std::cout << "The memory address of agePtr is " << agePtr << std::endl;
-    std::cout << "The memory address of agePtr is " << *agePtr << std::endl;
-
+    std::cout << "The value address of agePtr is " << *agePtr << std::endl;
+    
     return 0;
 }
