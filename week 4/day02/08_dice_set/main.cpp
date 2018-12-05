39800e188ad96b4c41e5e16f34448e7de9759d77
VeghFruzsi <vefruzso88@gmail.com>
2018-12-05 15:38:57 +0100

    ready

--- week 4/day02/08_dice_set/main.cpp
+++ week 4/day02/08_dice_set/main.cpp
@@ -9,8 +9,28 @@ int main(int argc, char* args[])
     // Check the current rolled numbers with getCurrent()
     // You can reroll with roll()
     // Your task is to roll the dices until all of the dices are 6
+    
     DiceSet diceSet;
     diceSet.roll();
+    int counter = 0;
+    std::vector<int> temp = diceSet.getCurrent();
+    bool allSix = false;
+
+    while (!allSix) {
+        if (temp[0] == 6 && temp[1] == 6 && temp[2] == 6 && temp[3] == 6 && temp[4] == 6 && temp[5] == 6) {
+            allSix = true;
+        }
+
+        std::cout << counter << ". roll: ";
+        for (int i = 0; i < 6; i++) {
+            std::cout << temp[i] << ' ';
+        }
+
+        diceSet.roll();
+        temp = diceSet.getCurrent();
+        counter++;
+        std::cout << std::endl;
+    }
 
     return 0;
 }
