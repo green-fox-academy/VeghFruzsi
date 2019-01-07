bb633a3b2892af68e42d5029bf9831bde8a0a61d
VeghFruzsi <vefruzso88@gmail.com>
2019-01-07 19:55:57 +0100

    ready

---
+++ week9/day02/main.c
@@ -0,0 +1,339 @@
+#include "stm32f7xx_hal.h"
+#include "stm32746g_discovery.h"
+
+
+static void SystemClock_Config(void);
+
+static void Error_Handler(void);
+
+static void MPU_Config(void);
+
+static void CPU_CACHE_Enable(void);
+
+GPIO_InitTypeDef led1;
+GPIO_InitTypeDef led2;
+GPIO_InitTypeDef led3;
+GPIO_InitTypeDef led4;
+
+int main(void) {
+
+	/* Configure the MPU attributes as Write Through */
+
+	MPU_Config();
+
+	/* Enable the CPU Cache */
+
+	CPU_CACHE_Enable();
+
+	HAL_Init();
+
+	/* Configure the System clock to have a frequency of 216 MHz */
+
+	SystemClock_Config();
+
+
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	led1.Pin = GPIO_PIN_0;
+	led1.Mode = GPIO_MODE_OUTPUT_PP;
+	led1.Pull = GPIO_PULLDOWN;
+	led1.Speed = GPIO_SPEED_HIGH;
+	HAL_GPIO_Init(GPIOA, &led1);
+
+	__HAL_RCC_GPIOF_CLK_ENABLE();
+	led2.Pin = GPIO_PIN_10;
+	led2.Mode =GPIO_MODE_OUTPUT_PP;
+	led2.Pull = GPIO_PULLDOWN;
+	led2.Speed = GPIO_SPEED_HIGH;
+	HAL_GPIO_Init(GPIOF, &led2);
+
+	led3.Pin = GPIO_PIN_9;
+	led3.Mode =GPIO_MODE_OUTPUT_PP;
+	led3.Pull = GPIO_PULLDOWN;
+	led3.Speed = GPIO_SPEED_HIGH;
+	HAL_GPIO_Init(GPIOF, &led3);
+
+	led4.Pin = GPIO_PIN_8;
+	led4.Mode =GPIO_MODE_OUTPUT_PP;
+	led4.Pull = GPIO_PULLDOWN;
+	led4.Speed = GPIO_SPEED_HIGH;
+	HAL_GPIO_Init(GPIOF, &led4);
+
+	BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
+	//BSP_PB_Init(BUTTON_WAKEUP, BUTTON_MODE_GPIO);
+
+	while (1) {
+
+			    /*HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
+			    HAL_Delay(1000);
+			    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+			    HAL_Delay(1000);
+
+			    HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_SET);
+			    HAL_Delay(1000);
+			    HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_RESET);
+			    HAL_Delay(1000);
+
+			    HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET);
+			    HAL_Delay(1000);
+			    HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET);
+			    HAL_Delay(1000);
+
+			    HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_SET);
+			    HAL_Delay(1000);
+			    HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_RESET);
+			    HAL_Delay(1000);*/
+
+			    if(BSP_PB_GetState(BUTTON_KEY)){
+				    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
+				    HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET);
+				    HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_RESET);
+				    HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_RESET);
+			    }else{
+			    	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_SET);
+			    	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_SET);
+			    	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+			    	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET);
+			    }
+		}
+}
+
+
+static void SystemClock_Config(void) {
+
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+
+	/* Enable HSE Oscillator and activate PLL with HSE as source */
+
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+
+	RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+
+	RCC_OscInitStruct.PLL.PLLM = 25;
+
+	RCC_OscInitStruct.PLL.PLLN = 432;
+
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+
+	RCC_OscInitStruct.PLL.PLLQ = 9;
+
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+
+		Error_Handler();
+
+	}
+
+
+	/* activate the OverDrive to reach the 216 Mhz Frequency */
+
+	if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
+
+		Error_Handler();
+
+	}
+
+
+	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+
+	 clocks dividers */
+
+	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK
+
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK) {
+
+		Error_Handler();
+
+	}
+
+}
+
+
+
+/**
+
+ * @brief  This function is executed in case of error occurrence.
+
+ * @param  None
+
+ * @retval None
+
+ */
+
+static void Error_Handler(void) {
+
+	/* User may add here some code to deal with this error */
+
+	while (1) {
+
+	}
+
+}
+
+
+
+/**
+
+ * @brief  Configure the MPU attributes as Write Through for SRAM1/2.
+
+ * @note   The Base Address is 0x20010000 since this memory interface is the AXI.
+
+ *         The Region Size is 256KB, it is related to SRAM1 and SRAM2  memory size.
+
+ * @param  None
+
+ * @retval None
+
+ */
+
+static void MPU_Config(void) {
+
+	MPU_Region_InitTypeDef MPU_InitStruct;
+
+
+
+	/* Disable the MPU */
+
+	HAL_MPU_Disable();
+
+
+
+	/* Configure the MPU attributes as WT for SRAM */
+
+	MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+
+	MPU_InitStruct.BaseAddress = 0x20010000;
+
+	MPU_InitStruct.Size = MPU_REGION_SIZE_256KB;
+
+	MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+
+	MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+
+	MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+
+	MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+
+	MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+
+	MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+
+	MPU_InitStruct.SubRegionDisable = 0x00;
+
+	MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+
+
+
+	HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+
+
+	/* Enable the MPU */
+
+	HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+
+}
+
+
+
+/**
+
+ * @brief  CPU L1-Cache enable.
+
+ * @param  None
+
+ * @retval None
+
+ */
+
+static void CPU_CACHE_Enable(void) {
+
+	/* Enable I-Cache */
+
+	SCB_EnableICache();
+
+
+
+	/* Enable D-Cache */
+
+	SCB_EnableDCache();
+
+}
+
+
+
+#ifdef  USE_FULL_ASSERT
+
+
+
+/**
+
+ * @brief  Reports the name of the source file and the source line number
+
+ *         where the assert_param error has occurred.
+
+ * @param  file: pointer to the source file name
+
+ * @param  line: assert_param error line source number
+
+ * @retval None
+
+ */
+
+void assert_failed(uint8_t* file, uint32_t line)
+
+{
+
+	/* User can add his own implementation to report the file name and line number,
+
+	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+
+
+	/* Infinite loop */
+
+	while (1)
+
+	{
+
+	}
+
+}
+
+#endif
+
+
+
+/**
+
+ * @}
+
+ */
+
+
+
+/**
+
+ * @}
+
+ */
+
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
